58f1205436816184b0cb51f834064e3bcd3b56bd
rolf <hrolfrc@gmail.com>
2023-08-06 15:28:18 -0600

    auc by feature

---
+++ examples/plot_auc_curve.py
@@ -0,0 +1,37 @@
+"""
+=============================
+Plot the AUC curve
+=============================
+
+An example plot of the auc as a function of features
+curve for :class:`CalfCV` on the breast cancer dataset.
+
+"""
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+
+from calfcv import CalfCV
+
+X, y = load_breast_cancer(return_X_y=True)
+cls = CalfCV().fit(X, y)
+
+xs = range(X.shape[1])
+fig, ax = plt.subplots()
+ax.plot(xs, cls.best_auc_)
+
+ax.set(xlabel='feature', ylabel='AUC',
+       title='AUC by feature weight')
+
+ax.grid()
+
+# zip joins x and y coordinates in pairs
+for x, y, z in zip(xs, cls.best_coef_, cls.best_auc_):
+    label = f"{y}"
+
+    plt.annotate(label,
+                 (x, z),
+                 textcoords="offset points",
+                 xytext=(0, 10),
+                 ha='center')
+
+plt.show()

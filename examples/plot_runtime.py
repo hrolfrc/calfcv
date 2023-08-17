12e5305cd84995f30fdaed42496a0103f2a9bdb7
rolf <hrolfrc@gmail.com>
2023-08-17 17:12:59 -0600

    added license

--- examples/plot_runtime.py
+++ examples/plot_runtime.py
@@ -8,6 +8,7 @@ A classifier runtime comparison plot including :class:`Calf`.
 Lasso is much faster than LogisticRegression, which in turn is faster than Calf.
 The calculation of AUC dominates the Calf runtime.
 
+Author: Rolf Carlson, Carlson Research LLC, <hrolfrc@gmail.com>, License: 3-clause BSD
 """
 import time
 
